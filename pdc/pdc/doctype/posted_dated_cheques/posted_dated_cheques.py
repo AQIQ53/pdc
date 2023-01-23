15808d332b034f5316d7b3e237ff403d63b2792c
AQIQ53 <98890538+AQIQ53@users.noreply.github.com>
2023-01-23 12:54:37 +0300

    edit on cancel

--- pdc/pdc/doctype/posted_dated_cheques/posted_dated_cheques.py
+++ pdc/pdc/doctype/posted_dated_cheques/posted_dated_cheques.py
@@ -7,5 +7,7 @@ from frappe.model.document import Document
 class PostedDatedCheques(Document):
 
 	def on_cancel(self):
+		self.docstatus=0
 		self.status = 'Cancelled'
 		self.save(ignore_permissions=True)
+		self.docstatus=2
